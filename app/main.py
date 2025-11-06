5914c9a58d290c96ef0bb32b2eee098259c14526
ferna <fernaobuenoleme@gmail.com>
2025-11-06 14:29:19 -0300

    Tentando de novo

--- app/main.py
+++ app/main.py
@@ -11,11 +11,11 @@ app = FastAPI(title="Projetos API", version="1.0.0")
 origins = [
     "http://localhost:5173", "http://127.0.0.1:5173",
     "http://localhost:5500", "http://127.0.0.1:5500"
-    "bdprojetos.azurewebsites.net",
+    "https://bdprojetos.azurewebsites.net",
 ]
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins, allow_credentials=True,
+    allow_origins= ".*", allow_credentials=True,
     allow_methods=["*"], allow_headers=["*"],
 )
 
